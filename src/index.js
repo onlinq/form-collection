7037463fb8871597bf63803483641393cb066b37
Tim Goudriaan <tim@onlinq.nl>
2021-02-02 15:28:09 +0100

    Add CSS custom properties with new CSS distribution for styling presets

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import './elements/collection';
 import './elements/collection-entry';
+
+import './stylesheets/inline.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,46 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'onlinq-collection.js',
-  },
-  devtool: false, /* disable devtool for web-test-runner */
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.html$/,
-        loader: 'raw-loader',
-      },
+module.exports = function(env, argv) {
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'onlinq-collection.js',
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'onlinq-collection.css',
+      }),
     ],
-  },
+    devtool: false, /* disable devtool for web-test-runner */
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.html$/,
+          loader: 'raw-loader',
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+          ],
+        },
+      ],
+    },
+    optimization: {
+      minimize: argv.mode === 'production',
+      minimizer: [
+        '...',
+        new CssMinimizerPlugin(),
+      ],
+    },
+  };
 };
