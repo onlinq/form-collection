98214c94e4d1b9026103c71b6679d63e76ecd3ef
Tim Goudriaan <tim@codedmonkey.com>
2023-01-27 09:57:24 +0100

    Add polyfill for custom elements in Safari

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import '@ungap/custom-elements';
+
 export {OnlinqFormCollectionElement} from './elements/collection';
 export {OnlinqFormCollectionEntryElement} from './elements/collection-entry';
 
